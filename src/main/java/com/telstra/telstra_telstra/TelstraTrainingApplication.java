b43afafb728a8929b4b06c8f76e5ca24603b18d4
avikatty <37908093+avikatty@users.noreply.github.com>
2018-04-17 17:01:52 +0530

    Create TelstraTrainingApplication.java

---
+++ src/main/java/com/telstra/telstra_telstra/TelstraTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.telstra.telstra_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelstraTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelstraTrainingApplication.class, args);
+	}
+}
