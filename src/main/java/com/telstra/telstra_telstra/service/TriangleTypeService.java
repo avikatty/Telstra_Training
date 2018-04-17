e89742b8745d8052ff28a1d1bff14f5d9980e8bd
avikatty <37908093+avikatty@users.noreply.github.com>
2018-04-17 17:09:55 +0530

    Create TriangleTypeService.java

---
+++ src/main/java/com/telstra/telstra_telstra/service/TriangleTypeService.java
@@ -0,0 +1,19 @@
+package com.telstra.telstra_training.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TriangleTypeService {
+
+	public String getTriangleType(int sideLength1, int sideLength2, int sideLength3) {
+
+		if (sideLength1 == sideLength2 && sideLength2 == sideLength3) {
+			return "Equilateral";
+		} else if (sideLength1 == sideLength2 || sideLength2 == sideLength3 || sideLength1 == sideLength3) {
+			return "Isosceles";
+		} else {
+			return "Scalene";
+		}
+	}
+
+}
